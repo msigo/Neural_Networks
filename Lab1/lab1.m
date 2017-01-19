ae2c7c4d4358a81e94b596d707a1ac4f8cd60a55
marc.sigo@hotmail.com <marcsigonius@n156-p47.kthopen.kth.se>
2017-01-19 13:22:15 +0100

    first commit

---
+++ Lab1/lab1.m
@@ -0,0 +1,7 @@
+classA(1,:) = randn(1,100) .* 0.5 + 1.0;
+classA(2,:) = randn(1,100) .* 0.5 + 0.5;
+classB(1,:) = randn(1,100) .* 0.5 - 1.0;
+classB(2,:) = randn(1,100) .* 0.5 + 0.0;
+
+patterns = [classA, classB];
+
