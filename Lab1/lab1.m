a63f08015ea03be5568dbdd837167879d32d0df3
SimonIgnat <Signat@kth.se>
2017-01-23 11:57:48 +0100

    Last question left

--- Lab1/lab1.m
+++ Lab1/lab1.m
@@ -90,3 +90,134 @@ plot (patterns(1, find(targets>0)), ...
     patterns(2, find(targets>0)), '*', ...
     patterns(1, find(targets<0)), ...
     patterns(2, find(targets<0)), '+');
+%%
+
+clc;
+clf;
+clear all;
+
+[patterns, targets] = nsepdata(200);
+[insize, ndata] = size(patterns);
+[outsize, ndata] = size(targets);
+
+
+
+hidden=40;
+eta = 0.1;
+alpha = 0.9;
+backprop_epoch = 100;
+
+W = 2*(rand(hidden,3) - 0.5*ones(hidden,3));
+V = 2*(rand(1,hidden+1) - 0.5*ones(1,hidden+1));
+dw = 0;
+dv = 0;
+
+
+for i = 1:backprop_epoch
+        plot_title = sprintf('Epoch = %f', i);
+              
+        [W,V,dw,dv,out] = backprop(W,V,dw,dv,patterns,targets,ndata,hidden,eta,alpha);
+        error(i) = sum(sum(abs(sign(out)- targets)./2));
+end
+figure(2)
+plot (error);
+title('Error');
+
+%% The encoder problem
+
+clc;
+clf;
+clear all;
+
+patterns = eye(8)*2 -1;
+targets = patterns;
+
+ndata = 8;
+
+nInputLayers = 8;
+hidden = 3;
+nOutputLayers = 8;
+
+W = 2*(rand(hidden,nInputLayers + 1) - 0.5*ones(hidden,nInputLayers +1 ));
+V = 2*(rand(nOutputLayers,hidden+1) - 0.5*ones(nOutputLayers,hidden+1));
+dw = 0;
+dv = 0;
+
+backprop_epoch = 400;
+eta = 0.4;
+alpha = 0.6;
+
+for i = 1:backprop_epoch
+        plot_title = sprintf('Epoch = %f', i);
+              
+        [W,V,dw,dv,out] = backprop(W,V,dw,dv,patterns,targets,ndata,hidden,eta,alpha);
+        error(i) = sum(sum(abs(sign(out)- targets)./2));
+end
+figure(2)
+plot (error);
+title('Error');
+
+
+
+%% Function Approximation
+
+
+clc;
+clf;
+clear all;
+
+x = [-5:1:5]';
+y = x;
+z = exp(-x.*x*0.1)*exp(-y.*y*0.1)' - 0.5;
+
+
+
+ndata = size(x,1)*size(y,1);
+gridsize = size(x,1);
+targets = reshape(z,1,ndata);
+[xx,yy] = meshgrid(x,y);
+patterns = [reshape(xx,1,ndata); reshape(yy,1,ndata)];
+
+nInputLayers = 2;
+hidden = 13;
+nOutputLayers = 1;
+
+W = 2*(rand(hidden,nInputLayers + 1) - 0.5*ones(hidden,nInputLayers +1 ));
+V = 2*(rand(nOutputLayers,hidden+1) - 0.5*ones(nOutputLayers,hidden+1));
+dw = 0;
+dv = 0;
+
+backprop_epoch = 200;
+eta = 0.1;
+alpha = 0.9;
+
+for i = 1:backprop_epoch
+        plot_title = sprintf('Epoch = %f', i);
+              
+        [W,V,dw,dv,out] = backprop(W,V,dw,dv,patterns,targets,ndata,hidden,eta,alpha);
+        
+        figure(2)
+        subplot(1,2,1)
+        zz = reshape(out, gridsize, gridsize);
+        mesh(x,y,zz);
+        axis([-5 5 -5 5 -0.7 0.7]);
+        title('Approximated function')
+        
+        subplot(1,2,2)
+        mesh(x,y,z)
+        axis([-5 5 -5 5 -0.7 0.7]); 
+        title('Target function')
+        
+        drawnow        
+end
+
+
+
+
+
+
+
+
+
+
+
--- Lab1/nsepdata.m
+++ Lab1/nsepdata.m
@@ -1,11 +1,11 @@
-function [patterns, targets] = nsepdata( )
-    classA(1,:) = [randn(1,50) .* 0.2 - 1.0, ...
-    randn(1,50) .* 0.2 + 1.0];
-    classA(2,:) = randn(1,100) .* 0.2 + 0.3;
-    classB(1,:) = randn(1,100) .* 0.3 + 0.0;
-    classB(2,:) = randn(1,100) .* 0.3 - 0.1;
+function [patterns, targets] = nsepdata(nData)
+    classA(1,:) = [randn(1,nData/4) .* 0.2 - 1.0, ...
+    randn(1,nData/4) .* 0.2 + 1.0];
+    classA(2,:) = randn(1,nData/2) .* 0.2 + 0.3;
+    classB(1,:) = randn(1,nData/2) .* 0.3 + 0.0;
+    classB(2,:) = randn(1,nData/2) .* 0.3 - 0.1;
 
     patterns = [classA, classB];
-    targets = [ones(1,100) -ones(1,100)];
+    targets = [ones(1,nData/2) -ones(1,nData/2)];
 end
 
