3b1be9709761582fe403c8ae577807363e6a9d03
SimonIgnat <Signat@kth.se>
2017-02-06 13:21:35 +0100

    Almost done

--- lab2/main.m
+++ lab2/main.m
@@ -94,6 +94,113 @@ itersub = 100;
 diter
 
 
+%% RBF Placement by Self Organization
+clear all, close all
+
+plotinit 
+
+data = read('cluster');
+
+units = 5;
+
+vqinit
+
+singlewinner = 1;
+
+%vqiter
+
+%%
+clear all, close all
+
+plotinit 
+
+data = read('cluster');
+
+units = 5;
+
+vqinit
+
+singlewinner = 0;
+
+vqiter
+
+
+%%
+
+clear all, close all
+
+plotinit 
+
+data = read('cluster');
+
+units = 15;
+
+vqinit
+
+singlewinner = 0;
+
+emiterb;
+
+
+%%
+clear all, close all
+
+
+plotinit 
+
+data = read('cluster');
+
+units = 10
+
+vqinit
+
+singlewinner = 1;
+
+emiterb
+
+
+%% Function Approximation
+
+clear all, close all
+
+lowpass = 0;
+
+[xtrain ytrain]=readxy('ballist',2,2);
+[xtest ytest]=readxy('balltest',2,2);
+
+if lowpass
+    xtrain = smooth(xtrain);
+    ytrain = smooth(ytrain);
+end
+
+units=10;
+data=xtrain;
+vqinit;
+singlewinner=1;
+emiterb
+
+Phi=calcPhi(xtrain,m,var);
+
+d1=ytrain(:,1);
+d2=ytrain(:,2);
+dtest1=ytest(:,1);
+dtest2=ytest(:,2);
+
+w1 = Phi\d1;
+w2 = Phi\d2;
+
+y1 = Phi*w1;
+y2 = Phi*w2;
+
+
+Phitest=calcPhi(xtest,m,var); 
+ytest1=Phitest*w1;
+ytest2=Phitest*w2;
+
+xyplot(d1,y1,'train1');
+xyplot(d2,y2,'train2');
+xyplot(dtest1,ytest1,'test1');
+xyplot(dtest2,ytest2,'test2');
 
 
 
